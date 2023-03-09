4888b3d3c2bf8d913078581989b8a92c6aaaf409
Alexander Matos <123131927+amatos-acc@users.noreply.github.com>
2023-03-09 01:45:43 +0000

    Midterm Question 1

--- src/question_1/main.cpp
+++ src/question_1/main.cpp
@@ -1,6 +1,29 @@
+#include<iostream>
+#include "question1.h"
+
+using std::cout;
+using std::cin;
 
 int main()
 {
+    auto num = 0;
+    auto response = 'y';
+    do
+    {
+        cout<<"Enter a number: ";
+        cin>>num;
 
+        if (num >= 1 && num <= 15) {
+            cout<<get_fib_sequence(num)<<"\n";
+        } else {
+            cout<<"Please enter a number between 1 and 15\n";
+        }
+        cout<<"Do you want to do it again? (y/n): ";
+        cin>>response;
+
+    } while (response == 'y' || response == 'Y');
+
+    cout<<"Exiting...\n";
+    
     return 0;
 }
