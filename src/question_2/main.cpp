dc13b9a51ad9bbe309d8e39495123f6353073307
Alexander Matos <123131927+amatos-acc@users.noreply.github.com>
2023-03-09 16:16:35 +0000

    Midterm Question 2

--- src/question_2/main.cpp
+++ src/question_2/main.cpp
@@ -1,6 +1,40 @@
+#include<iostream>
+#include "question2.h"
+
+using std::cout;
+using std::cin;
 
 int main()
 {
+    auto num1 = 0;
+    auto num2 = 0;
+    auto result = 0;
+    auto response = 'y';
+    do
+    {
+        cout<<"Enter number one: ";
+        cin>>num1;
 
+        cout<<"Enter number two: ";
+        cin>>num2;
+
+        if (num1 >= 1 && num1 <= 200) {
+            if (num2 >= 1 && num2 <= 200) {
+                result = find_gcd(num1, num2);
+                cout<<"The GCD is: "<<result<<"\n";
+            } else {
+                cout<<"Please enter a number between 1 and 200\n";
+            }
+        } else {
+            cout<<"Please enter a number between 1 and 200\n";
+        }
+        
+        cout<<"Do you want to do it again? (y/n): ";
+        cin>>response;
+
+    } while (response == 'y' || response == 'Y');
+
+    cout<<"Exiting...\n";
+    
     return 0;
 }
--- test/question_test_2/question_tests_2.cpp
+++ test/question_test_2/question_tests_2.cpp
@@ -6,7 +6,9 @@ TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
-TEST_CASE("test")
+TEST_CASE("Question 2 Test")
 {
-	REQUIRE(test_config() == true);
+	REQUIRE(find_gcd(15, 25) == 5);
+	REQUIRE(find_gcd(16, 32) == 16);
+	REQUIRE(find_gcd(159, 309) == 3);
 }
